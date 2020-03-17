2f78f279f0b70acc7075f6eafe002a77f3723450
liyu0915 <961554151@qq.com>
2020-03-17 15:43:27 +0800

    Initial Commit

---
+++ src/pages/test11/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="counter-warp">
+      <i-panel>
+        <view style="text-align:center">鲜花</view>
+      </i-panel>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
